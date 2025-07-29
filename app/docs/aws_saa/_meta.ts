201be5b6ff87cb45b1a1052111bf0118ff16b07c
Daniel <daniel.guo@vivalabs.com.au>
2025-07-29 20:54:51 +1000

    Add aws serverless notes

--- app/docs/aws_saa/_meta.ts
+++ app/docs/aws_saa/_meta.ts
@@ -3,7 +3,10 @@ import { MetaRecord } from "nextra";
 const meta: MetaRecord = {
   ec2_essential: "EC2 Essentials",
   ec2_ha: "EC2 High Availability",
-  ec2_storage: "EC2 Storage"
+  ec2_storage: "EC2 Storage",
+  serverless_ecs: "Serverless ECS",
+  serverless_lambda: "Serverless Lambda",
+  serverless_eks: "Serverless EKS",
 };
 
 export default meta;
