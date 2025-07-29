5e475e980b578ebbdaf8f54c9f8df28db6f64f9a
Daniel <daniel.guo@vivalabs.com.au>
2025-07-30 07:31:43 +1000

    Add blogs to docs for now

--- app/docs/_meta.ts
+++ app/docs/_meta.ts
@@ -4,6 +4,7 @@ const meta: MetaRecord= {
   index: "Overview",
   java: "Java",
   aws_saa: "AWS SAA",
+  blogs: "Blogs",
 }
 
 export default meta;
---
+++ app/docs/blogs/_meta.ts
@@ -0,0 +1,8 @@
+import { MetaRecord } from "nextra";
+
+const meta: MetaRecord = {
+  'deploy_nodejs_application': "Deploy Node.js Application to AWS",
+  'awesome_mac_apps': "Awesome Mac Apps",
+}
+
+export default meta;
