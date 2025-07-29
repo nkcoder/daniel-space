5b28e64263458d5f24943e883b71c5d7e987cada
Daniel <daniel.guo@vivalabs.com.au>
2025-07-29 21:19:22 +1000

    Add aws network notes

--- app/docs/aws_saa/_meta.ts
+++ app/docs/aws_saa/_meta.ts
@@ -7,11 +7,16 @@ const meta: MetaRecord = {
   serverless_ecs: "Serverless ECS",
   serverless_lambda: "Serverless Lambda",
   serverless_eks: "Serverless EKS",
-  storage_s3: "S3",
-  storage_rds: "RDS",
-  storage_aurora: "Aurora",
-  storage_dynamodb: "DynamoDB",
-  storage_elasticache: "ElastiCache",
+  storage_s3: "Storage - S3",
+  storage_rds: "Storage - RDS",
+  storage_aurora: "Storage - Aurora",
+  storage_dynamodb: "Storage - DynamoDB",
+  storage_elasticache: "Storage - ElastiCache",
+  network_vpc: "Network - VPC",
+  network_eni: "Network - ENI",
+  network_networking: "Network - Networking",
+  network_route53: "Network - Route 53",
+  network_security: "Network - Security",
 };
 
 export default meta;
