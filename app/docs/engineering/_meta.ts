63d0c5cb018196ec63e5d8b54383c8f4956da736
Daniel <daniel.guo@vivalabs.com.au>
2025-08-06 11:13:09 +1000

    Move iac tool to docs

--- app/docs/engineering/_meta.ts
+++ app/docs/engineering/_meta.ts
@@ -3,6 +3,7 @@ import { MetaRecord } from "nextra";
 const meta: MetaRecord = {
   adr_introduction: "ADR Introduction",
   code_review_why_how: "Code Review: Why & How",
+  choose_an_iac_tool: "Choose an IaC Tool",
   pull_request_template: "Pull Request Template"
 };
 
