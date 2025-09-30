43ed64f57869ecb9266098852698620f4e5a262a
Daniel <daniel.guo@vivalabs.com.au>
2025-09-30 19:32:02 +1000

    Add node.js doc: type vs interface

--- app/docs/nodejs/_meta.ts
+++ app/docs/nodejs/_meta.ts
@@ -5,6 +5,7 @@ const meta: MetaRecord = {
   migrate_from_npm_to_pnpm: 'Migrate from npm to pnpm',
   understanding_event_loop: 'Understanding Node.js Event Loop',
   use_biome_in_new_projects: 'Use Biome in New Node.js Projects',
-  memory_management: 'Memory Management in Node.js'
+  memory_management: 'Memory Management in Node.js',
+  type_vs_interface: '`type` vs `interface` in TypeScript'
 };
 export default meta;
