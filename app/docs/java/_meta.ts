d21b2d0ce246ad3f134ac548546538bd2be091d7
Daniel <daniel.guo@vivalabs.com.au>
2025-12-07 12:39:39 +1100

    Save the java meta data file

--- app/docs/java/_meta.ts
+++ app/docs/java/_meta.ts
@@ -1,7 +1,7 @@
 import { MetaRecord } from 'nextra';
 
 const meta: MetaRecord = {
-  lambda_effective_final: '',
+  lambda_effective_final: 'Effective Final in Lambdas',
   virtual_threads: 'Introduction to Virtual Threads'
 };
 
